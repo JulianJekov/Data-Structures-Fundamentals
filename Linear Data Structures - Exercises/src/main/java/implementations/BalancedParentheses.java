1b717c82a473107b433dce01ac84e09e107cbcec
Julian Jekov <drmsz@abv.bg>
2024-10-09 21:07:50 +0300

    ArrayDeque implementation

--- Linear Data Structures - Exercises/src/main/java/implementations/BalancedParentheses.java
+++ Linear Data Structures - Exercises/src/main/java/implementations/BalancedParentheses.java
@@ -11,6 +11,32 @@ public class BalancedParentheses implements Solvable {
 
     @Override
     public Boolean solve() {
-        return null;
+        ArrayDeque<Character> openBrackets = new ArrayDeque<>();
+        boolean isBalanced = true;
+        for (int i = 0; i < this.parentheses.length(); i++) {
+            char currentBracket = this.parentheses.charAt(i);
+
+            if (currentBracket == '(' || currentBracket == '{' || currentBracket == '[') {
+                openBrackets.push(currentBracket);
+            } else {
+                if (openBrackets.isEmpty()) {
+                    isBalanced = false;
+                    break;
+                }
+                char lastOpenBracket = openBrackets.pop();
+                if (currentBracket == ')' && lastOpenBracket != '(') {
+                    isBalanced = false;
+                    break;
+                } else if (currentBracket == '}' && lastOpenBracket != '{') {
+                    isBalanced = false;
+                    break;
+                } else if (currentBracket == ']' && lastOpenBracket != '[') {
+                    isBalanced = false;
+                    break;
+                }
+            }
+
+        }
+        return isBalanced;
     }
 }
