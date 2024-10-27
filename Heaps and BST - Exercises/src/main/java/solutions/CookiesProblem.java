67840394a1efbc68920efc01ac6219691513e08a
Julian Jekov <drmsz@abv.bg>
2024-10-27 17:17:32 +0200

    Cookies Problem

--- Heaps and BST - Exercises/src/main/java/solutions/CookiesProblem.java
+++ Heaps and BST - Exercises/src/main/java/solutions/CookiesProblem.java
@@ -1,7 +1,26 @@
 package solutions;
 
+import java.util.*;
+
 public class CookiesProblem {
-    public Integer solve(int k, int[] cookies) {
-        return null;
+    public Integer solve(int requiredSweetness, int[] cookies) {
+        Queue<Integer> cookiesQueue = new PriorityQueue<>();
+        for (int cookie : cookies) {
+            cookiesQueue.add(cookie);
+        }
+        int currentMinSweetness = cookiesQueue.peek();
+        int steps = 0;
+        while (currentMinSweetness < requiredSweetness && cookiesQueue.size() > 1) {
+            steps++;
+            int leastSweetCookie = cookiesQueue.poll();
+            int secondLeastSweetCookie = cookiesQueue.poll();
+
+            int combinedSweetCookie = leastSweetCookie + 2 * secondLeastSweetCookie;
+
+            cookiesQueue.add(combinedSweetCookie);
+
+            currentMinSweetness = cookiesQueue.peek();
+        }
+        return currentMinSweetness > requiredSweetness ? steps : -1;
     }
 }
